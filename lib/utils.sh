508163393fd7300f24e8fe32eb33ae3b328084a5
Javier Tiá <javier.tia@gmail.com>
2015-03-15 08:00:46 -0600

    utils.sh: remove extract function, no longer need

--- lib/utils.sh
+++ lib/utils.sh
@@ -22,44 +22,6 @@ mvdir()
     && rm -rf $1
 }
 
-# Extract different compress files
-extract()
-{
-  local e=0 i c
-  for i; do
-    if [[ -r $i ]]; then
-      c=''
-      case $i in
-        *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
-          c='bsdtar xvf' ;;
-        *.7z)
-          c='7z x'       ;;
-        *.Z)
-          c='uncompress' ;;
-        *.bz2)
-          c='bunzip2'    ;;
-        *.exe)
-          c='cabextract' ;;
-        *.gz)
-          c='gunzip'     ;;
-        *.rar)
-          c='unrar x'    ;;
-        *.xz)
-          c='unxz'       ;;
-        *.zip)
-          c='unzip'      ;;
-        *)
-          echo "$0: cannot extract \`$i': Unrecognized file extension" >&2; e=1 ;;
-      esac
-
-      [[ $c ]] && command $c "$i"
-    else
-      echo "$0: cannot extract \`$i': File is unreadable" >&2; e=2
-    fi
-  done
-  return $e
-}
-
 # Repeat n times command
 repeat_cmd()
 {
