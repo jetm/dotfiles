352dc18c6957725ab7ed71e5b9cb90254e70e3e1
Javier Tiá <javier.tia@gmail.com>
2015-12-17 18:22:56 -0600

    ycm_extra_conf.py: use C language, instead of C++

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -50,7 +50,8 @@ flags = [
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x', 'c++',
+#  '-x', 'c++',
+'-x', 'c',
 # For current project
 '-I', 'include',
 ]
