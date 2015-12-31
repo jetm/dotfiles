395346cd5ef0203e2e94ef3c692aec879cda1ea4
Javier Tiá <javier.tia@gmail.com>
2015-12-30 19:19:23 -0600

    ycm_extra_conf.py: add extra indentation spaces and newlines

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -35,9 +35,10 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
+  '-Wall',
+  '-Wextra',
 #  '-Werror',
+
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
@@ -45,15 +46,17 @@ flags = [
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
 #  '-std=c++11',
-'-std=gnu11',
+  '-std=gnu11',
+
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
 #  '-x', 'c++',
-'-x', 'c',
+  '-x', 'c',
+
 # For current project
-'-I', 'include',
+  '-I', 'include',
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
