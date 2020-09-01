2abce7a2bea4df74f803c7fdc799bcb5e14939b9
Javier Tiá <javier.tia@hpe.com>
2020-09-01 09:21:14 -0700

    Cleanup enhancd

--- .zsh/60-enhancd.zsh
+++ .zsh/60-enhancd.zsh
@@ -3,7 +3,7 @@ compinit
 source ~/repos/enhancd/init.sh
 
 # export ENHANCD_FILTER=$(available_filter ${FILTER})
-export ENHANCD_FILTER="peco:fzf"
+# export ENHANCD_FILTER="peco:fzf"
 # export ENHANCD_DOT_SHOW_FULLPATH=1
 # export ENHANCD_DISABLE_HOME=0
 # export ENHANCD_DOT_ARG=...
