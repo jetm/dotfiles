bdebb333e2b30db1e785ab775d302df11c7477c2
Javier Tiá <javier.tia@hpe.com>
2020-09-01 09:20:00 -0700

    Add less settings

--- .zsh/01-zsh-init.zsh
+++ .zsh/01-zsh-init.zsh
@@ -25,3 +25,18 @@ setopt MULTIOS
 
 # Make Vi mode transitions faster (KEYTIMEOUT is in hundredths of a second)
 export KEYTIMEOUT=1
+
+#
+# Less
+#
+
+# Set the default Less options.
+# Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
+# Remove -X and -F (exit if the content fits on one screen) to enable it.
+export LESS='-F -g -i -M -R -S -w -X -z-4'
+
+# Set the Less input preprocessor.
+# Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
+if (( $#commands[(i)lesspipe(|.sh)] )); then
+  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
+fi
