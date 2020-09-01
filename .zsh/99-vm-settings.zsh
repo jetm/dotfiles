34ae6cc22f68f6040c27f06563453a27b4ad8cf0
Javier Tiá <javier.tia@hpe.com>
2020-09-01 09:21:32 -0700

    Cleanup 99-vm-settings

--- .zsh/99-vm-settings.zsh
+++ .zsh/99-vm-settings.zsh
@@ -18,39 +18,10 @@ export EDITOR="nvim"
 
 alias gerrit-code-nos="ssh -p 29418 javier.tia@code-nos.rose.rdlabs.hpecorp.net"
 
-# Remove duplicates PATH
-remove_PATH_duplicates() {
-    if [ -n "$PATH" ]; then
-        old_PATH=$PATH:; PATH=
-        while [ -n "$old_PATH" ]; do
-            x=${old_PATH%%:*}      # the first remaining entry
-            case $PATH: in
-                *:"$x":*) ;;         # already there
-                *) PATH=$PATH:$x;;   # not there yet
-            esac
-            old_PATH=${old_PATH#*:}
-        done
-        PATH=${PATH#:}
-        unset old_PATH x
-        export PATH
-    fi
-}
-
 if [[ $(lsb_release -i) = *Ubuntu* ]]; then
-   source /aruba/halon/infra/halon.profile
-   remove_PATH_duplicates
-   # unalias bb
+  source /aruba/halon/infra/halon.profile
+  remove_PATH_duplicates
+  # unalias bb
 fi
-
-function mvI() {
-  if [ "$#" -ne 1 ] || [ ! -f "$1" ]; then
-    command mv "$@"
-    return
-  fi
-
-  newfilename="$1"
-  vared newfilename
-  command mv -v -- "$1" "$newfilename"
-}
 
 # vim:set ts=2 sw=2 et:
