bd39c56ac239dfa82e87d9036dd4270537f322f8
Javier Tiá <javier.tia@gmail.com>
2020-11-19 08:31:32 -0600

    Clean up

--- .zsh/03-paths.zsh
+++ .zsh/03-paths.zsh
@@ -15,14 +15,11 @@ NPM_PACKAGES="${HOME}/.npm-packages"
 add_path $NPM_PACKAGES/bin
 
 add_path $HOME/bin
+
 # Preserve MANPATH if you already defined it somewhere in your config.
 # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
 export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 
-if [ -d "$HOME"/local/share/man ] ; then
-  if test -n "$MANPATH" ; then
-    MANPATH=~/local/share/man:"${MANPATH}"
-  else
-    MANPATH=~/local/share/man
-  fi
+if [ -d "${HOME}/go" ]; then
+  export GOPATH=${HOME}/go
 fi
---
+++ bin/unmute-mic.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+qdbus org.kde.kglobalaccel /component/kmix invokeShortcut "mic_mute"
