4c5f4c4a021d8851f902bc4a5e7b8a636bc20b01
Javier Tiá <javier.tia@hpe.com>
2021-06-24 08:32:33 -0700

    zshrc,nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 local vim = vim
 local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
 local g = vim.g -- a table to access global variables
-local opt = vim.opt -- to set options
+local o = vim.o -- to set options
 
 --
 -- Colors
@@ -14,7 +14,7 @@ if exists('+termguicolors')
 endif
 ]]
 
-opt.background = 'dark'
+o.background = 'dark'
 cmd [[colorscheme onedark]]
 
 --
@@ -25,37 +25,37 @@ require('plugins')
 --
 -- General settings
 --
-opt.autoindent = true -- Do smart autoindenting when starting a new line
-opt.autowrite = true -- Automatically write a file when leaving a modified buffer
-opt.cursorline = true -- Highlight current line
-opt.emoji = true -- Enable emojis
-opt.hidden = true -- Allow buffer switching without saving
-opt.ignorecase = true
-opt.linebreak = true -- Do not break words
-opt.linespace = 0 -- No extra spaces between rows
-opt.matchtime = 0 -- Hide matching time
-opt.mouse = 'a' -- Enable mouse scrolling
-opt.number = true
-opt.regexpengine = 1 -- Use the old engine, new is for debugging
-opt.relativenumber = true -- Enable numbers on the left, current line is 0
-opt.ruler = true -- Show the ruler
-opt.shiftround = true -- Round indent to multiple of 'shiftwidth'
-opt.shiftwidth = 4
-opt.shortmess = 'atOI' -- No help Uganda information, and overwrite read messages to avoid PRESS ENTER prompts
-opt.showcmd = true -- Show partial commands in status line and Selected characters/lines in visual mode
-opt.showmatch = true -- Show matching brackets/parentthesis
-opt.showmode = true -- Show current mode in command-line
-opt.showtabline = 2 -- Always show tabline
-opt.smartcase = true -- Case insensitive search, but case sensitive when uc present
-opt.smartindent = true
-opt.smarttab = true -- Tab key actions
-opt.expandtab = true
-opt.softtabstop = 4
-opt.tabstop = 4 -- Tab width
+o.autoindent = true -- Do smart autoindenting when starting a new line
+o.autowrite = true -- Automatically write a file when leaving a modified buffer
+o.cursorline = true -- Highlight current line
+o.emoji = true -- Enable emojis
+o.hidden = true -- Allow buffer switching without saving
+o.ignorecase = true
+o.linebreak = true -- Do not break words
+o.linespace = 0 -- No extra spaces between rows
+o.matchtime = 0 -- Hide matching time
+o.mouse = 'a' -- Enable mouse scrolling
+o.number = true
+o.regexpengine = 1 -- Use the old engine, new is for debugging
+o.relativenumber = true -- Enable numbers on the left, current line is 0
+o.ruler = true -- Show the ruler
+o.shiftround = true -- Round indent to multiple of 'shiftwidth'
+o.shortmess = 'atOI' -- No help Uganda information, and overwrite read messages to avoid PRESS ENTER prompts
+o.showcmd = true -- Show partial commands in status line and Selected characters/lines in visual mode
+o.showmatch = true -- Show matching brackets/parentthesis
+o.showmode = true -- Show current mode in command-line
+o.showtabline = 2 -- Always show tabline
+o.smartcase = true -- Case insensitive search, but case sensitive when uc present
+o.smartindent = true
+o.smarttab = true -- Tab key actions
+o.expandtab = true
+o.softtabstop = 4
+o.shiftwidth = o.softtabstop
+o.tabstop = o.softtabstop -- Tab width
 
 -- Display whitespace characters
-opt.list = true
-opt.listchars = {tab = '→ ', trail = '·', nbsp = '␣'}
+o.list = true
+vim.opt.listchars = {tab = '→ ', trail = '·', nbsp = '␣'}
 
 -- I like to include the end newline
 -- cmd 'set clipboard+=unnamedplus' -- Copy paste between vim and everything else
@@ -104,93 +104,85 @@ cmd [[
 --
 g.mapleader = ' '
 
-local opts = {expr = true}
-
--- local utils = require('utils')
-local mp = require('utils.map')
--- local inoremap = mp.inoremap
-local nmap, vmap, xmap, imap, smap, nnoremap = mp.nmap, mp.vmap, mp.xmap,
-                                               mp.imap, mp.smap, mp.nnoremap
+local vimp = require('vimp')
 
 --  Helper for saving file
-nmap('<C-s>', ':update<CR>')
-vmap('<C-s>', '<C-c>:update<CR>')
-imap('<C-s>', '<C-o>:update<CR>')
+vimp.nmap('<C-s>', ':update<CR>')
+vimp.vmap('<C-s>', '<C-c>:update<CR>')
+vimp.imap('<C-s>', '<C-o>:update<CR>')
 
 --
 -- moving up and down work as you would expect
-nnoremap('j', 'gj', {silent = true})
-nnoremap('k', 'gk', {silent = true})
+vimp.nnoremap('j', 'gj')
+vimp.nnoremap('k', 'gk')
 
 -- move one line up and one line down
-nnoremap('<M-j>', ':move +1<CR>', {silent = true})
-nnoremap('<M-k>', ':move -2<CR>', {silent = true})
+vimp.nnoremap('<M-j>', ':move +1<CR>')
+vimp.nnoremap('<M-k>', ':move -2<CR>')
 
-nnoremap('Q', ':q<CR>', {silent = true})
+vimp.nnoremap('Q', ':q<CR>')
 
 -- Stop being so lazy - remove arrow key config
-nmap('<UP>', '<Nop>')
-nmap('<Down>', '<Nop>')
-nmap('<Left>', '<Nop>')
-nmap('<Right>', '<Nop>')
-imap('<UP>', '<Nop>')
-imap('<Down>', '<Nop>')
-imap('<Left>', '<Nop>')
-imap('<Right>', '<Nop>')
+vimp.nmap('<UP>', '<Nop>')
+vimp.nmap('<Down>', '<Nop>')
+vimp.nmap('<Left>', '<Nop>')
+vimp.nmap('<Right>', '<Nop>')
+vimp.imap('<UP>', '<Nop>')
+vimp.imap('<Down>', '<Nop>')
+vimp.imap('<Left>', '<Nop>')
+vimp.imap('<Right>', '<Nop>')
 
 -- copy until the end
-nnoremap('Y', 'y$', {silent = true})
+vimp.nnoremap('Y', 'y$')
 
 -- Keep the cursor in place while joining lines
-nnoremap('J', 'mzJ`z', {silent = true})
+vimp.nnoremap('J', 'mzJ`z')
 
 -- Visual shifting (does not exit Visual mode)
-vmap('<', '<gv')
-vmap('>', '>gv')
+vimp.vmap('<', '<gv')
+vimp.vmap('>', '>gv')
 
 -- Move between vim panes with Ctrl-J
-nnoremap('<C-J>', '<C-W>j', {silent = true})
-nnoremap('<C-K>', '<C-W>k', {silent = true})
-nnoremap('<C-L>', '<C-W>l', {silent = true})
-nnoremap('<C-H>', '<C-W>h', {silent = true})
+vimp.nnoremap('<C-J>', '<C-W>j')
+vimp.nnoremap('<C-K>', '<C-W>k')
+vimp.nnoremap('<C-L>', '<C-W>l')
+vimp.nnoremap('<C-H>', '<C-W>h')
 
 -- CtrlP compatibility
 -- fzf.vim is quicker than fzf.preview
-nnoremap('<C-p>',
-         ':Telescope find_files find_command=fd,--hidden,--exclude,.git<CR>',
-         {silent = true})
+vimp.nnoremap('<C-p>',
+              ':Telescope find_files find_command=fd,--hidden,--exclude,.git<CR>')
 
 -- Use <Alt-l> to clear the highlighting of :set hlsearch.
-nnoremap('<M-l>',
-         ':nohlsearch<CR>:diffupdate<CR>:syntax sync fromstart<CR><C-l>',
-         {silent = true})
+vimp.nnoremap('<M-l>',
+              ':nohlsearch<CR>:diffupdate<CR>:syntax sync fromstart<CR><C-l>')
 
 -- Comments
-nmap('<leader>c', '<Plug>NERDCommenterToggle')
-vmap('<leader>c', '<Plug>NERDCommenterToggle')
+vimp.nmap('<leader>c', '<Plug>NERDCommenterToggle')
+vimp.vmap('<leader>c', '<Plug>NERDCommenterToggle')
 
 --
 -- barbar
 --
-nnoremap('<leader>1', ':BufferGoto1<CR>', {silent = true})
-nnoremap('<leader>2', ':BufferGoto2<CR>', {silent = true})
-nnoremap('<leader>3', ':BufferGoto3<CR>', {silent = true})
-nnoremap('<leader>4', ':BufferGoto4<CR>', {silent = true})
-nnoremap('<leader>5', ':BufferGoto5<CR>', {silent = true})
-nnoremap('<leader>6', ':BufferGoto6<CR>', {silent = true})
-nnoremap('<leader>7', ':BufferGoto7<CR>', {silent = true})
-nnoremap('<leader>8', ':BufferGoto8<CR>', {silent = true})
-nnoremap('<leader>9', ':BufferGoto9<CR>', {silent = true})
+vimp.nnoremap('<leader>1', ':BufferGoto1<CR>')
+vimp.nnoremap('<leader>2', ':BufferGoto2<CR>')
+vimp.nnoremap('<leader>3', ':BufferGoto3<CR>')
+vimp.nnoremap('<leader>4', ':BufferGoto4<CR>')
+vimp.nnoremap('<leader>5', ':BufferGoto5<CR>')
+vimp.nnoremap('<leader>6', ':BufferGoto6<CR>')
+vimp.nnoremap('<leader>7', ':BufferGoto7<CR>')
+vimp.nnoremap('<leader>8', ':BufferGoto8<CR>')
+vimp.nnoremap('<leader>9', ':BufferGoto9<CR>')
 
 -- barbar buffer close still has some issues. Try later
-nnoremap('<leader>q', ':BufferClose<CR>', {silent = true})
+vimp.nnoremap('<leader>q', ':BufferClose<CR>')
 -- nnoremap <silent> <leader>q :Bwipeout<CR>
 
 --
 -- NvimTree
 --
-nnoremap('<leader>tt', ':NvimTreeToggle<CR>', {silent = true})
-nnoremap('<leader>tf', ':NvimTreeFindFile<CR>', {silent = true})
+vimp.nnoremap('<leader>tt', ':NvimTreeToggle<CR>')
+vimp.nnoremap('<leader>tf', ':NvimTreeFindFile<CR>')
 
 --
 -- shellharden
@@ -201,28 +193,26 @@ nnoremap('<leader>tf', ':NvimTreeFindFile<CR>', {silent = true})
 -- vsnip
 --
 -- Expand
-imap('<C-j>', "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", opts)
-smap('<C-j>', "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", opts)
+-- imap('<C-j>', "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", opts)
+-- smap('<C-j>', "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'", opts)
 
 --
 -- expand_region
 --
-xmap("v", "<Plug>(expand_region_expand)")
-xmap("V", "<Plug>(expand_region_shrink)")
+vimp.xmap("v", "<Plug>(expand_region_expand)")
+vimp.xmap("V", "<Plug>(expand_region_shrink)")
 
 --
 -- hlslens
 --
-nnoremap("n",
-         "<cmd>execute('normal! ' . v:count1 . 'n')<CR><cmd>lua require('hlslens').start()<CR>",
-         {silent = true})
-nnoremap("N",
-         "<cmd>execute('normal! ' . v:count1 . 'N')<CR><cmd>lua require('hlslens').start()<CR>",
-         {silent = true})
-nnoremap("*", "*<cmd>lua require('hlslens').start()<CR>")
-nnoremap("#", "#<cmd>lua require('hlslens').start()<CR>")
-nnoremap("g*", "g*<cmd>lua require('hlslens').start()<CR>")
-nnoremap("g#", "g#<cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("n",
+              "<cmd>execute('normal! ' . v:count1 . 'n')<CR><cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("N",
+              "<cmd>execute('normal! ' . v:count1 . 'N')<CR><cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("*", "*<cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("#", "#<cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("g*", "g*<cmd>lua require('hlslens').start()<CR>")
+vimp.nnoremap("g#", "g#<cmd>lua require('hlslens').start()<CR>")
 
 --
 -- Specific Language settings
