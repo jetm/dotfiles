1558ad6b3cd87d831b36b99deee439d50dcb415e
Javier Tiá <javier.tia@gmail.com>
2021-08-22 18:04:26 -0600

    coq

--- nvim/lua/plugins/coq.lua
+++ nvim/lua/plugins/coq.lua
@@ -1,10 +1,8 @@
 local vim = vim
 
 vim.g.coq_settings = {
-    auto_start = true,
+    auto_start = 'shut-up',
     ['clients.tabnine.enabled'] = true,
     ['clients.tmux.enabled'] = false,
     ['clients.tags.enabled'] = false
 }
-
-vim.cmd [[autocmd VimEnter * COQnow -s]]
