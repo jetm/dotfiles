92c13b8a6b1a97c3399c7e9f4862301a01935750
Javier Tiá <javier.tia@hpe.com>
2021-09-18 10:22:12 -0700

    99-vm-settings

--- .zsh/99-vm-settings.zsh
+++ .zsh/99-vm-settings.zsh
@@ -1,7 +1,8 @@
 #
 # VM settings
 #
-HPE_PROXY=http://web-proxy.rose.hpecorp.net:8080
+# houston is quicker than rose. Rose sometimes doesn't work
+HPE_PROXY=http://web-proxy.houston.hpecorp.net:8080
 
 # All variables in lowercase
 export http_proxy="${HPE_PROXY}"
@@ -12,10 +13,11 @@ export ftp_proxy="${HPE_PROXY}"
 export HTTP_PROXY="${HPE_PROXY}"
 export HTTPS_PROXY="${HPE_PROXY}"
 export FTP_PROXY="${HPE_PROXY}"
+
+# This could be better if some arubanetworks.com hosts are outside of HPE
 # export no_proxy="127.0.0.1,localhost,rose.rdlabs.hpecorp.net,in.rdlabs.hpecorp.net,sg.rdlabs.hpecorp.net,stash.arubanetworks.com,jira.arubanetworks.com,confluence.arubanetworks.com,acp-ci.arubanetworks.com,cloudcop.arubathena.com"
 export no_proxy="127.0.0.1,localhost,hpecorp.net,arubanetworks.com,arubathena.com,hpe.com"
-
-alias gerrit-code-nos="ssh -p 29418 javier.tia@code-nos.rose.rdlabs.hpecorp.net"
+export NO_PROXY="${no_proxy}"
 
 if [[ $(lsb_release -i) = *Ubuntu* ]]; then
   source /aruba/halon/infra/halon.profile
