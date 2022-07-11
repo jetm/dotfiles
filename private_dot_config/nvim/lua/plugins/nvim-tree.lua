cfcf06f47bb993b1e32f78caeb4b0f174760d2c1
Javier Tiá <javier.tia@hpe.com>
2022-07-11 09:55:58 -0700

    nvim-tree

--- private_dot_config/nvim/lua/plugins/nvim-tree.lua
+++ private_dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -4,36 +4,5 @@ if not ok then
 	return
 end
 
-local ok2, icons = pcall(require, "nvim-nonicons")
-if not ok2 then
-	error("Loading nvim-nonicons")
-	return
-end
-
-local vim = vim
-
-vim.g.nvim_tree_icons = {
-	default = icons.get("file"),
-	symlink = icons.get("file-symlink"),
-	git = {
-		unstaged = "✗",
-		staged = "✓",
-		unmerged = "",
-		renamed = "➜",
-		untracked = "★",
-		deleted = "",
-		ignored = "◌",
-	},
-	folder = {
-		arrow_open = "",
-		arrow_closed = "",
-		default = icons.get("file-directory"),
-		open = icons.get("file-directory"),
-		empty = icons.get("file-directory-outline"),
-		empty_open = icons.get("file-directory-outline"),
-		symlink = icons.get("file-submodule"),
-		symlink_open = icons.get("file-submodule"),
-	},
-}
-
-nvim_tree.setup({})
+nvim_tree.setup({
+})
