f04cea39d5756e334edc9455b7ce59a0c9973f4e
Javier Tia <javier.tia@gmail.com>
2023-02-20 19:51:34 -0600

    fzf.zsh: remove most of unused config
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_dot_zsh/40-fzf.zsh
+++ exact_dot_zsh/40-fzf.zsh
@@ -10,15 +10,15 @@ FZF_DEFAULT_OPTS+=' --prompt="∼ "'
 FZF_DEFAULT_OPTS+=' --pointer="▶"'
 FZF_DEFAULT_OPTS+=' --marker="✓"'
 FZF_DEFAULT_OPTS+=' --bind "?:toggle-preview"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-a:select-all"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-d:deselect-all"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-t:toggle-all"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-r:reload(eval $FZF_DEFAULT_COMMAND)"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-y:execute-silent(echo {+} | xargs -n1 tmux set-buffer)"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-e:execute(nvim {} > /dev/tty)+abort"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-j:preview-page-down"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-k:preview-page-up"'
-FZF_DEFAULT_OPTS+=' --bind "ctrl-o:execute-multi(nvim -p {} > /dev/tty)"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-a:select-all"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-d:deselect-all"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-t:toggle-all"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-r:reload(eval $FZF_DEFAULT_COMMAND)"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-y:execute-silent(echo {+} | xargs -n1 tmux set-buffer)"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-e:become(nvim {} > /dev/tty)+abort"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-j:preview-page-down"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-k:preview-page-up"'
+# FZF_DEFAULT_OPTS+=' --bind "ctrl-o:execute-multi(nvim -p {} > /dev/tty)"'
 export FZF_DEFAULT_OPTS
 export FZF_COMPLETION_TRIGGER="@@"
 
@@ -32,8 +32,8 @@ FORGIT_FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS}"
 FORGIT_FZF_DEFAULT_OPTS+=' --preview-window=:down:80%:nohidden'
 export FORGIT_FZF_DEFAULT_OPTS
 
-FORGIT_DIFF_FZF_OPTS+=' --bind="ctrl-e:execute(nvim {-1} > /dev/tty)"'
-export FORGIT_DIFF_FZF_OPTS
+# FORGIT_DIFF_FZF_OPTS+=' --bind="ctrl-e:become(nvim {-1} > /dev/tty)"'
+# export FORGIT_DIFF_FZF_OPTS
 
 # Show proper colors and not %F{yellow}
 zstyle -d ':completion:*' format
