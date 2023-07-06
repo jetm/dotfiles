a81b26b67cf613341f266124a0f6978db6b826b0
Javier Tia <javier.tia@gmail.com>
2023-07-06 14:55:24 -0600

    neovim: introduce yep.map()
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_config/mappings.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/mappings.lua
@@ -1,39 +1,22 @@
 --
 -- Misc mappings
 --
-
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
-
-
 -- quitting mapping
-map({ "n", "x" }, "q", ":BufDel<CR>", { silent = true })
-map({ "n", "x" }, "Q", ":qa<CR>", { silent = true })
-
--- expand_region
-map({ "x" }, "v", "<Plug>(expand_region_expand)", { silent = true })
-map({ "x" }, "V", "<Plug>(expand_region_shrink)", { silent = true })
+yet.map({ "n", "x" }, "q", ":BufDel<CR>", { silent = true })
+yet.map({ "n", "x" }, "Q", ":qa<CR>", { silent = true })
 
 -- Visual shifting (does not exit Visual mode)
-map({ "v" }, "<", "<gv", { silent = true })
-map({ "v" }, ">", ">gv", { silent = true })
-map({ "n" }, "<", "<<_", { silent = true })
-map({ "n" }, ">", ">>_", { silent = true })
+yet.map({ "v" }, "<", "<gv", { silent = true })
+yet.map({ "v" }, ">", ">gv", { silent = true })
+yet.map({ "n" }, "<", "<<_", { silent = true })
+yet.map({ "n" }, ">", ">>_", { silent = true })
 
--- vimp lacks of unmap() feature
-map({ "n", "v" }, "+", "<Plug>(dial-increment)")
-map({ "n", "v" }, "-", "<Plug>(dial-increment)")
+-- increment/decrement anything
+yet.map({ "n", "v" }, "+", "<Plug>(dial-increment)")
+yet.map({ "n", "v" }, "-", "<Plug>(dial-increment)")
 
 -- formatter
-map(
+yet.map(
   { "n" },
   "<leader>f",
   "<cmd>lua vim.lsp.buf.format()<CR>",
@@ -42,72 +25,34 @@ map(
 
 -- Comments
 -- Ctrl-/ as VSCode and Jetbrain
-map({ "n", "v" }, "<c-_>", "<Plug>NERDCommenterToggle", { silent = true })
+yet.map({ "n", "v" }, "<c-_>", "<Plug>NERDCommenterToggle", { silent = true })
 
 --
 -- bufferline
 --
-map({ "n" }, "<leader>1", ":BufferLineGoToBuffer 1<CR>", { silent = true })
-map({ "n" }, "<leader>2", ":BufferLineGoToBuffer 2<CR>", { silent = true })
-map({ "n" }, "<leader>3", ":BufferLineGoToBuffer 3<CR>", { silent = true })
-map({ "n" }, "<leader>4", ":BufferLineGoToBuffer 4<CR>", { silent = true })
-map({ "n" }, "<leader>5", ":BufferLineGoToBuffer 5<CR>", { silent = true })
-map({ "n" }, "<leader>6", ":BufferLineGoToBuffer 6<CR>", { silent = true })
-map({ "n" }, "<leader>7", ":BufferLineGoToBuffer 7<CR>", { silent = true })
-map({ "n" }, "<leader>8", ":BufferLineGoToBuffer 8<CR>", { silent = true })
-map({ "n" }, "<leader>9", ":BufferLineGoToBuffer 9<CR>", { silent = true })
+yet.map({ "n" }, "<leader>1", ":BufferLineGoToBuffer 1<CR>", { silent = true })
+yet.map({ "n" }, "<leader>2", ":BufferLineGoToBuffer 2<CR>", { silent = true })
+yet.map({ "n" }, "<leader>3", ":BufferLineGoToBuffer 3<CR>", { silent = true })
+yet.map({ "n" }, "<leader>4", ":BufferLineGoToBuffer 4<CR>", { silent = true })
+yet.map({ "n" }, "<leader>5", ":BufferLineGoToBuffer 5<CR>", { silent = true })
+yet.map({ "n" }, "<leader>6", ":BufferLineGoToBuffer 6<CR>", { silent = true })
+yet.map({ "n" }, "<leader>7", ":BufferLineGoToBuffer 7<CR>", { silent = true })
+yet.map({ "n" }, "<leader>8", ":BufferLineGoToBuffer 8<CR>", { silent = true })
+yet.map({ "n" }, "<leader>9", ":BufferLineGoToBuffer 9<CR>", { silent = true })
 
 --
 -- Ctrl <C-> Mappings
 --
 --  Helper for saving file
-map({ "n" }, "<C-s>", ":update<CR>", { silent = true })
-map({ "v" }, "<C-s>", "<C-c>:update<CR>", { silent = true })
-map({ "i" }, "<C-s>", "<C-o>:update<CR>", { silent = true })
+yet.map({ "n" }, "<C-s>", ":update<CR>", { silent = true })
+yet.map({ "v" }, "<C-s>", "<C-c>:update<CR>", { silent = true })
+yet.map({ "i" }, "<C-s>", "<C-o>:update<CR>", { silent = true })
 
--- Search for words
-map({ "n", "x", "o" }, "s", "<Cmd>Svart<CR>", { silent = true })
-
-map("n", "_", require("oil").open, { desc = "Open parent directory" })
+yet.map("n", "_", require("oil").open, { desc = "Open parent directory" })
 
 --
 -- <F1>..<F12> Mappings
 --
--- NvimTree
--- map({ "n" }, "<F2>", ":NvimTreeToggle<CR>", { silent = true })
-
--- Shift + <F2>
--- map({ "n" }, "<S-F2>", ":NvimTreeFindFile<CR>", { silent = true })
 
 -- F5 shows a diagnostics
-map({ "n" }, "<F5>", ":TroubleToggle<CR>", { silent = true })
-
--- Use the w, e, b motions like a spider. Move by subwords and skip insignificant punctuation
---
--- map(
---   { "n", "o", "x" },
---   "w",
---   "<cmd>lua require('spider').motion('w')<CR>",
---   { desc = "Spider-w" }
--- )
-
--- map(
---   { "n", "o", "x" },
---   "e",
---   "<cmd>lua require('spider').motion('e')<CR>",
---   { desc = "Spider-w" }
--- )
---
--- map(
---   { "n", "o", "x" },
---   "b",
---   "<cmd>lua require('spider').motion('b')<CR>",
---   { desc = "Spider-w" }
--- )
---
--- map(
---   { "n", "o", "x" },
---   "ge",
---   "<cmd>lua require('spider').motion('ge')<CR>",
---   { desc = "Spider-w" }
--- )
+yet.map({ "n" }, "<F5>", ":TroubleToggle<CR>", { silent = true })
--- private_dot_config/exact_nvim/exact_lua/exact_config/utils.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/utils.lua
@@ -1,8 +1,8 @@
+_G.yet = {}
+
 -- For debugging purpose
 -- :lua put(...)
-local vim = vim
-
-function _G.put(...)
+function yet.put(...)
   local objects = {}
   for i = 1, select("#", ...) do
     local v = select(i, ...)
@@ -13,13 +13,24 @@ function _G.put(...)
   return ...
 end
 
-vim.notify = function(msg, log_level, _)
-  if msg:match("exit code") then
-    return
-  end
-  if log_level == vim.log.levels.ERROR then
-    vim.api.nvim_err_writeln(msg)
-  else
-    vim.api.nvim_echo({ { msg } }, true, {})
+function yet.map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
   end
 end
+
+-- vim.notify = function(msg, log_level, _)
+--   if msg:match("exit code") then
+--     return
+--   end
+--   if log_level == vim.log.levels.ERROR then
+--     vim.api.nvim_err_writeln(msg)
+--   else
+--     vim.api.nvim_echo({ { msg } }, true, {})
+--   end
+-- end
