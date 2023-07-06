d6fd18c50d8c7ac054b0fde1cfbeee307196bbef
Javier Tia <javier.tia@gmail.com>
2023-07-06 15:01:16 -0600

    neovim: report error when loading modules
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/init.lua
+++ private_dot_config/exact_nvim/init.lua
@@ -2,9 +2,17 @@ if vim.g.vscode then
   return
 end
 
-require("config.global")
-require("config.lazy")
-require("config.autocmds")
-require("config.backup")
-require("config.mappings")
-require("config.utils")
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+
+for _, source in ipairs {
+
+"config.utils",
+"config.global",
+"config.lazy",
+"config.autocmds",
+"config.backup",
+"config.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
