fa2b089119f44663125cc39ede26446227df8cab
Javier Tia <javier.tia@gmail.com>
2023-07-07 20:34:48 -0600

    neovim: rewrite nvim-autopairs settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-autopairs.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-autopairs.lua
@@ -1,7 +1,7 @@
 -- Vim plugin, insert or delete brackets, parens, quotes in pair
 local M = {
   "windwp/nvim-autopairs",
-  event = "VeryLazy",
+  event = "InsertEnter",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
 
@@ -12,43 +12,7 @@ function M.config()
     return
   end
 
-  nvim_autopairs.setup({
-    active = true,
-    on_config_done = nil,
-    ---@usage  modifies the function or method delimiter by filetypes
-    map_char = {
-      all = "(",
-      tex = "{",
-    },
-    ---@usage check bracket in same line
-    enable_check_bracket_line = false,
-    ---@usage check treesitter
-    check_ts = true,
-    disable_filetype = { "TelescopePrompt" },
-    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
-    enable_moveright = true,
-    ---@usage disable when recording or executing a macro
-    disable_in_macro = false,
-    ---@usage add bracket pairs after quote
-    enable_afterquote = true,
-    ---@usage map the <BS> key
-    map_bs = true,
-    ---@usage map <c-w> to delete a pair if possible
-    map_c_w = false,
-    ---@usage disable when insert after visual block mode
-    disable_in_visualblock = false,
-    fast_wrap = {
-      map = "<M-e>",
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-      offset = 0, -- Offset from pattern match
-      end_key = "$",
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      check_comma = true,
-      highlight = "Search",
-      highlight_grey = "Comment",
-    },
-  })
+  nvim_autopairs.setup({})
 
   local ok2, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
   if not ok2 then
@@ -56,7 +20,10 @@ function M.config()
     return
   end
 
-  nvim_treesitter_configs.setup({ autopairs = { enable = true } })
+  -- If you want insert `(` after select function or method item
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  local cmp = require("cmp")
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
