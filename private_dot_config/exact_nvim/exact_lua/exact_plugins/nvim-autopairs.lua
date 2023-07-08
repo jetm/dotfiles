f002e9c78fec6df173410af4de1fe1adea5fa888
Javier Tia <javier.tia@gmail.com>
2023-07-08 15:30:41 -0600

    neovim: rewrite nvim-autopairs settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-autopairs.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-autopairs.lua
@@ -2,7 +2,12 @@
 local M = {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  dependencies = {
+    {
+      "hrsh7th/nvim-cmp",
+      event = { "InsertEnter", "CmdlineEnter" },
+    },
+  },
 }
 
 function M.config()
@@ -14,15 +19,47 @@ function M.config()
 
   nvim_autopairs.setup({})
 
-  local ok2, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
-  if not ok2 then
-    error("Loading nvim-treesitter.configs")
-    return
-  end
-
   -- If you want insert `(` after select function or method item
   local cmp_autopairs = require("nvim-autopairs.completion.cmp")
   local cmp = require("cmp")
+  local handlers = require("nvim-autopairs.completion.handlers")
+
+  cmp.event:on(
+    "confirm_done",
+    cmp_autopairs.on_confirm_done({
+      filetypes = {
+        -- "*" is a alias to all filetypes
+        ["*"] = {
+          ["("] = {
+            kind = {
+              cmp.lsp.CompletionItemKind.Function,
+              cmp.lsp.CompletionItemKind.Method,
+            },
+            handler = handlers["*"],
+          },
+        },
+        lua = {
+          ["("] = {
+            kind = {
+              cmp.lsp.CompletionItemKind.Function,
+              cmp.lsp.CompletionItemKind.Method,
+            },
+            ---@param char string
+            ---@param item table item completion
+            ---@param bufnr number buffer number
+            ---@param rules table
+            ---@param commit_character table<string>
+            handler = function(char, item, bufnr, rules, commit_character)
+              -- Your handler function. Inpect with print(vim.inspect{char, item, bufnr, rules, commit_character})
+            end,
+          },
+        },
+        -- Disable for tex
+        tex = false,
+      },
+    })
+  )
+
   cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
