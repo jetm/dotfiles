8ae0abc9a1ebedc701eedfe2c92d0cce943e6836
Javier Tia <javier.tia@gmail.com>
2023-07-07 20:35:19 -0600

    neovim: rewrite nvim-colorizer settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-colorizer.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/nvim-colorizer.lua
@@ -1,5 +1,5 @@
 local M = {
-  "norcalli/nvim-colorizer.lua",
+  "NvChad/nvim-colorizer.lua",
   lazy = true,
 }
 
@@ -11,15 +11,7 @@ function M.config()
   end
 
   -- Attaches to every FileType mode
-  colorizer.setup({ "*" }, {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    RRGGBBAA = true, -- #RRGGBBAA hex codes
-    rgb_fn = true, -- CSS rgb() and rgba() functions
-    hsl_fn = true, -- CSS hsl() and hsla() functions
-    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  })
+  colorizer.setup({ "*" })
 end
 
 return M
