71afaee2b6953e47e39d0d8286f25fd9b8412d75
Javier Tia <javier.tia@gmail.com>
2023-07-09 21:30:58 -0600

    neovim: rewrite oil settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/oil.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/oil.lua
@@ -2,21 +2,18 @@ local M = {
   -- File explorer
   "stevearc/oil.nvim",
   cmd = "Oil",
-  event = "BufEnter",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`
-      -- Conflict with pathogen
-      default_file_explorer = false,
-      columns = {
-        "icon",
-        "permissions",
-        "size",
-        "mtime",
-      },
-    })
-  end,
+  opts = {
+    -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`
+    -- Conflict with pathogen
+    default_file_explorer = false,
+    columns = {
+      "icon",
+      "permissions",
+      "size",
+      "mtime",
+    },
+  },
 }
 
 return M
