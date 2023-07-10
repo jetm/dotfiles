638199aa573d8e663fd156faf205d882603659a9
Javier Tia <javier.tia@gmail.com>
2023-07-09 21:21:43 -0600

    neovim: rewrite comment-nvim settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/comment-nvim.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/comment-nvim.lua
@@ -1,22 +1,13 @@
 -- Vim plugin for intensely nerdy commenting powers
-local M = {
-  'numToStr/Comment.nvim',
-  event = {"BufRead", "BufNewFile"},
-}
-
-function M.config()
-  local ok, comment = pcall(require, "Comment")
-  if not ok then
-    error("Loading comment")
-    return
-  end
-
-  comment.setup({
+M = {
+  "numToStr/Comment.nvim",
+  event = { "BufRead", "BufNewFile" },
+  opts = {
     mappings = {
-        basic = false,
-        extra = false,
+      basic = false,
+      extra = false,
     },
-  })
-end
+  }
+}
 
 return M
--- private_dot_config/exact_nvim/exact_lua/exact_plugins/suda.lua
+++
@@ -1,6 +0,0 @@
--- An alternative to sudo.vim
-return {
-  -- Suda
-  "lambdalisue/suda.vim",
-  config = vim.api.nvim_set_var("suda_smart_edit", 1),
-}
