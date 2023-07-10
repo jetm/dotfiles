b49e21f72c2c8acd4937aa6733f763eaa0eb321a
Javier Tia <javier.tia@gmail.com>
2023-07-09 21:22:02 -0600

    neovim: rewrite dressing settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/dressing.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/dressing.lua
@@ -10,7 +10,18 @@ function M.config()
     return
   end
 
-  dressing.setup({})
+  dressing.setup({
+    input = {
+        default_prompt = "➤ ",
+        win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" },
+      },
+      select = {
+        backend = { "telescope", "builtin" },
+        builtin = {
+          win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" },
+        },
+      },
+  })
 end
 
 return M
