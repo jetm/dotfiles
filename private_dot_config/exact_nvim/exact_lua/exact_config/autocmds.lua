13bc04db8b16a8db45c07cf77f3c7ff3bf2659a4
Javier Tia <javier.tia@gmail.com>
2023-07-22 17:08:53 -0600

    autocmds

--- private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -1,5 +1,5 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("jetm_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup("yet_" .. name, { clear = true })
 end
 
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
@@ -26,6 +26,7 @@ autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
     "PlenaryTestPopup",
+    "nofile",
     "floaterm",
     "help",
     "lspinfo",
