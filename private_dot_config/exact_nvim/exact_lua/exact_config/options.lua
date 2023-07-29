74b35c7954d43507ffa4777957ba0da7607f17b6
Javier Tia <javier.tia@gmail.com>
2023-07-28 19:25:01 -0600

    options

--- private_dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -46,7 +46,6 @@ o.undodir = os.getenv("HOME") .. "/.vim_data/undofile" -- Where to save undofile
 o.updatetime = 300 -- Length of time to wait before triggering the plugin
 o.virtualedit = "block" -- allow going past end of line in visual block mode
 o.writebackup = false -- Disable making a backup before overwriting a file
-o.spelllang = { "en" } -- English dictionary
 
 local g = vim.g -- Global set options
 g.mapleader = " " -- set leader key
