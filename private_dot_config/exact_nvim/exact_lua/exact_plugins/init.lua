c9be9d4f5f1263d43af169a1a9bc3545c010b97a
Javier Tia <javier.tia@gmail.com>
2023-10-16 10:54:54 -0600

    nvim: add mini.indentscope to highlight indent
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -382,6 +382,44 @@ return {
     config = true,
   },
 
+  -- Active indent guide and indent text objects. When you're browsing
+  -- code, this highlights the current level of indentation
+  {
+    "echasnovski/mini.indentscope",
+    event = { "BufReadPost", "BufNewFile" },
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+      -- remove animation. It's distracting
+      draw = {
+        delay = 0,
+        animation = function ()
+          return 0
+        end
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
+
   -- Align text interactively
   -- keys: gas and gAs (preview)
   {
