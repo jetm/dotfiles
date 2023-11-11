ffacfb9f6f5e7957131e7aa7d4e960dca7127cb6
Javier Tia <javier.tia@gmail.com>
2023-11-11 10:57:49 -0600

    nvim: add clangd lsp settings
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lsp_conf.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lsp_conf.lua
@@ -34,6 +34,7 @@ return function(_, _)
   require("mason-lspconfig").setup({
     ensure_installed = {
       "bashls",
+      "clangd",
       "dockerls",
       "jsonls",
       "lua_ls",
@@ -106,6 +107,35 @@ return function(_, _)
     },
   })
 
+  lspconfig.clangd.setup({
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+          "lspconfig.util"
+        ).find_git_ancestor(fname)
+      end,
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+  })
   local lsp = require("lsp-zero").preset({})
 
   lsp.on_attach(function(_, bufnr)
@@ -226,5 +256,4 @@ return function(_, _)
       format = require("lspkind").cmp_format({ with_text = false }),
     },
   })
-
 end
