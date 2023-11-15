2736206e01b9267174777c0f9635b1346ec18430
Javier Tia <javier.tia@gmail.com>
2023-11-15 10:34:17 -0600

    owrt-ci-build: refresh build flow
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_bin/owrt-ci-build.sh
+++ exact_bin/owrt-ci-build.sh
@@ -3,25 +3,13 @@
 # ./scripts/feeds update -a
 # ./scripts/feeds install -a
 
-# echo CONFIG_ALL=y >> .config
-# echo CONFIG_TARGET_MULTI_PROFILE=y >> .config
-# echo CONFIG_TARGET_PER_DEVICE_ROOTFS=y >> .config
-# echo CONFIG_TARGET_ALL_PROFILES=y >> .config
-# echo "CONFIG_TARGET_armsr=y" >> .config
-# echo "CONFIG_TARGET_armsr_armv7=y" >> .config
-#
-# make defconfig
-#
-# make tools/tar/compile -j$(nproc)
-#
-# make package/download package/check FIXUP=1 -j$(nproc)
-#
-# make tools/install -j$(nproc)
-
 rm -f .config
 
 echo CONFIG_ALL=y >> .config
 echo CONFIG_TARGET_armsr=y >> .config
+echo CONFIG_TARGET_MULTI_PROFILE=y >> .config
+echo CONFIG_TARGET_PER_DEVICE_ROOTFS=y >> .config
+echo CONFIG_TARGET_ALL_PROFILES=y >> .config
 # echo CONFIG_TARGET_armsr_armv7=y >> .config
 # echo CONFIG_TARGET_DEVICE_armsr_armv7_DEVICE_generic=y >> .config
 echo CONFIG_TARGET_armsr_armv8=y >> .config
@@ -41,5 +29,9 @@ echo CONFIG_MOLD=y >> .config
 
 make defconfig
 
-make toolchain/install -j$(nproc)
-make target/compile -j$(nproc)
+# make clean
+make -j $(nproc) tools/tar/compile
+# make -j $(nproc) package/download package/check FIXUP=1
+make -j $(nproc) tools/install
+make -j $(nproc) toolchain/install
+make -j $(nproc) target/compile
