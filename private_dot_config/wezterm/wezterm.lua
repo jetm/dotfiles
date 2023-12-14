a1ad3fe65eb94ae4f0d0e0b7ab91c5159a456c93
Javier Tia <javier.tia@gmail.com>
2023-12-14 11:08:45 -0600

    wezterm: rework configuration
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,102 +1,36 @@
 local wezterm = require("wezterm")
+wezterm.log_info("reloading")
 
--- Show the domain and workspace in the status area, for quicker and easier sanity checking
-wezterm.on("update-right-status", function(window, pane)
-  window:set_right_status(
-    pane:get_domain_name() .. " / " .. window:active_workspace()
-  )
-end)
+local config = wezterm.config_builder()
 
-function attach_to_workspace(name, window, pane)
-  window:perform_action(
-    wezterm.action({ SwitchToWorkspace = { name = name } }),
-    pane
-  )
+config.scrollback_lines = 9999999
 
-  -- brief delay while SwitchToWorkspace finishes setting up the window
-  wezterm.sleep_ms(250)
+config.default_prog = { "tmux", "attach", "-t", "vm" }
 
-  window:perform_action(wezterm.action({ AttachDomain = name }), pane)
-end
+config.color_scheme = "OneHalfDark"
+config.font = wezterm.font("Noto Mono Nerd Font")
+config.font_size = 11
+config.warn_about_missing_glyphs = false
 
-return {
-  -- font = wezterm.font 'Fira Code',
-  font_size = 11.1,
-  color_scheme = "OneHalfDark",
-  enable_csi_u_key_encoding = true,
-  use_fancy_tab_bar = true,
-  disable_default_key_bindings = true,
-  -- timeout_milliseconds defaults to 1000 and can be omitted
-  leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
-  keys = {
-    { key = "e", mods = "LEADER", action = wezterm.action.ShowLauncher },
-    {
-      key = "|",
-      mods = "LEADER|SHIFT",
-      action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "-",
-      mods = "LEADER",
-      action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "h",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Left"),
-    },
-    {
-      key = "l",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Right"),
-    },
-    {
-      key = "z",
-      mods = "LEADER",
-      action = wezterm.action.TogglePaneZoomState,
-    },
-    {
-      key = "c",
-      mods = "LEADER",
-      action = wezterm.action.CopyTo("Clipboard"),
-    },
-    {
-      key = "v",
-      mods = "SUPER",
-      action = wezterm.action.PasteFrom("Clipboard"),
-    },
-    {
-      key = "t",
-      mods = "LEADER",
-      action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-    },
-    -- {key="t", mods="CTRL|SHIFT", action=wezterm.action.SpawnTab("CurrentPaneDomain")},
-    {
-      key = "x",
-      mods = "LEADER",
-      action = wezterm.action.CloseCurrentTab({ confirm = true }),
-    },
-    { key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
-    { key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
-    { key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
-    { key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
-    { key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
-    { key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
-    { key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
-    { key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
-    { key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
-    { key = "a", mods = "LEADER", action = wezterm.action.ActivateLastTab },
-    {
-      key = "f",
-      mods = "LEADER",
-      action = wezterm.action.Search({ CaseSensitiveString = "" }),
-    },
-  },
-  scrollback_lines = 999999,
-  unix_domains = {
-    {
-      name = "dropdown",
-    },
-  },
-  -- default_gui_startup_args = { 'connect', 'dropdown' },
-}
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+
+config.default_gui_startup_args = { "connect", "dropdown" }
+config.unix_domains = { { name = "dropdown" } }
+
+config.disable_default_key_bindings = true
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = "https://www.github.com/$1/$3",
+})
+
+return config
