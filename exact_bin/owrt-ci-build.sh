eac6cd19409434497bc8fcfddb9d01065168c8d2
Javier Tia <javier.tia@gmail.com>
2023-12-15 17:58:02 -0600

    owrt-ci-build: reduce config
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_bin/owrt-ci-build.sh
+++ exact_bin/owrt-ci-build.sh
@@ -1,39 +1,24 @@
 # shellcheck disable=SC2148
 # Update the feeds
-./scripts/feeds update -a
-./scripts/feeds install -a
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
 
 rm -f .config
 
 echo CONFIG_TARGET_armsr=y >> .config
-echo CONFIG_TARGET_MULTI_PROFILE=y >> .config
-echo CONFIG_TARGET_PER_DEVICE_ROOTFS=y >> .config
-echo CONFIG_TARGET_ALL_PROFILES=y >> .config
 # echo CONFIG_TARGET_armsr_armv7=y >> .config
-# echo CONFIG_TARGET_armsr_armv7_DEVICE_generic=y >> .config
-# echo CONFIG_TARGET_DEVICE_armsr_armv7_DEVICE_generic=y >> .config
 echo CONFIG_TARGET_armsr_armv8=y >> .config
-echo CONFIG_TARGET_armsr_armv8_DEVICE_generic=y >> .config
-echo CONFIG_TARGET_DEVICE_armsr_armv8_DEVICE_generic=y >> .config
-# echo CONFIG_TARGET_ALL_PROFILES=y >> .config
-# echo CONFIG_TARGET_PER_DEVICE_ROOTFS=y >> .config
-# echo CONFIG_TARGET_MULTI_PROFILE=y >> .config
 
-echo CONFIG_KERNEL_WERROR=y >> .config
-echo CONFIG_ALL_KMODS=y >> .config
 echo CONFIG_DEVEL=y >> .config
+echo CONFIG_BUILD_LOG=y >> .config
+echo 'CONFIG_BUILD_LOG_DIR="/tmp/build_logs"' >> .config
 echo CONFIG_CCACHE=y >> .config
 echo 'CONFIG_CCACHE_DIR="/home/tiamarin/repos/work/openwrt/.ccache"' >> .config
 
-echo CONFIG_BUILD_NLS=y >> .config
-echo CONFIG_AUTOREBUILD=y >> .config
-echo CONFIG_BUILD_LOG=y >> .config
-echo 'CONFIG_BUILD_LOG_DIR="/tmp/build_logs"' >> .config
-echo CONFIG_MOLD=y >> .config
-
 make defconfig
 
-# make clean
+# make -j "$(nproc)" clean download world
+
 # make -j $(nproc) tools/tar/compile
 # make -j $(nproc) package/download package/check FIXUP=1
 # make -j $(nproc) tools/install
