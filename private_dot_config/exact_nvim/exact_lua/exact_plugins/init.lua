97512d41b389c37d296e11154a9f9c86a7a90f61
Javier Tia <javier.tia@gmail.com>
2023-12-16 10:58:55 -0600

    kitty: nvim: try a less dark background
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -57,14 +57,13 @@ return {
     lazy = false,
     -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
-    opts = {
-      caching = true,
-    },
     config = function()
       require("onedarkpro").setup({
+        caching = true,
         colors = {
-          onedark = { bg = "#1c2025", black = "#1c2025" },
-          -- onedark = { bg = "#15181C", black = "#15181C" }, -- darker
+          onedark = { bg = "#23272E", black = "#23272E" },
+          -- onedark = { bg = "#1c2025", black = "#1c2025" }, -- darker
+          -- onedark = { bg = "#15181C", black = "#15181C" }, -- darker plus
         },
       })
       vim.cmd.colorscheme("onedark")
