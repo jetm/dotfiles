f028d818b53616825afc31624de16683bbd14335
Javier Tia <javier.tia@gmail.com>
2024-01-05 11:28:21 -0600

    neovim: cleanup

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -782,19 +782,18 @@ return {
     event = "BufReadPre",
   },
 
-  --
+  -- 😽 Open your Kitty scrollback buffer with Neovim
   {
     "mikesmithgh/kitty-scrollback.nvim",
     enabled = true,
     lazy = true,
     cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
     event = { "User KittyScrollbackLaunch" },
-    -- version = '*', -- latest stable version, may have breaking changes if major version changed
-    -- version = '^2.0.0', -- pin major version, include fixes and features that do not have breaking changes
     config = function()
       require("kitty-scrollback").setup()
     end,
   },
+
   -- {
   --   "sindrets/diffview.nvim",
   --   dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
--- private_dot_config/exact_nvim/init.lua
+++ private_dot_config/exact_nvim/init.lua
@@ -2,10 +2,6 @@ if vim.g.vscode then
   return
 end
 
-if vim.loader and vim.fn.has("nvim-0.9.1") == 1 then
-  vim.loader.enable()
-end
-
 -- Manually add new filetypes
 vim.filetype.add({
   filename = {},
