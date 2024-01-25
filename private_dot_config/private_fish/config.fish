d63aac5c498830e64f67ee586a753f1d3190c26f
Javier Tia <javier.tia@gmail.com>
2024-01-25 17:29:01 -0600

    fish: vi-mode is not ready
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
-if status is-interactive
+if ! status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# fish is not ready yet. Lack of good vi mode
+set -U fish_greeting
+set -Ux EDITOR nvim
+fish_vi_key_bindings insert
