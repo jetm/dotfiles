0912590a5533dcbc68251409178b03e99abebb28
Javier Tia <javier.tia@gmail.com>
2024-01-31 11:39:03 -0600

    zsh: disable p10k-instant-prompt
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_dot_zsh/01-p10k-instant-prompt.zsh
+++ exact_dot_zsh/01-p10k-instant-prompt.zsh
@@ -2,9 +2,9 @@
 # Stay close to the top of ~/.zshrc
 
 # shellcheck disable=SC2296
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  # shellcheck disable=SC2296
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   # shellcheck disable=SC2296
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
 
 # vim:ft=sh ts=2 sw=2 et:
