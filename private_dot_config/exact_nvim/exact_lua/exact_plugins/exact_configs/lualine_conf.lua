e4d2af8ea957291687f99ed074d27dc8618de78d
Javier Tia <javier.tia@gmail.com>
2024-02-17 11:45:16 -0600

    nvim: rework indent size
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lualine_conf.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lualine_conf.lua
@@ -13,8 +13,10 @@ return function(_, opts)
     padding = 0,
   }
 
-  local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  local indent_size = function()
+    local indent_type = vim.api.nvim_buf_get_option(0, "expandtab") and "spaces" or "tab"
+    local indent_size = vim.api.nvim_buf_get_option(0, "tabstop")
+    return ("%s: %s"):format(indent_type, indent_size)
   end
 
   require("lualine").setup({
@@ -31,9 +33,8 @@ return function(_, opts)
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "diff" },
-      -- lualine_b = { "branch", "diff" },
       lualine_c = { diagnostics },
-      lualine_x = { spaces, "encoding", "filetype", "fileformat" },
+      lualine_x = { indent_size, "encoding", "filetype", "fileformat" },
       lualine_y = { location },
       lualine_z = { "progress" },
     },
