e9848ec1fbbc67fa510dacf3ed135bfb769205cb
Javier Tia <javier.tia@gmail.com>
2024-02-27 03:40:35 -0600

    Remove fish confs
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/private_fish/config.fish
+++
@@ -1,8 +0,0 @@
-if ! status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# fish is not ready yet. Lack of good vi mode
-set -U fish_greeting
-set -Ux EDITOR nvim
-fish_vi_key_bindings insert
