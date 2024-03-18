39e1a72dca314255fd8ee40e00d99749d1f81bf5
Javier Tia <javier.tia@gmail.com>
2024-03-17 20:42:33 -0600

    nvim: fix livegrep mapping
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_config/mappings.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/mappings.lua
@@ -43,7 +43,7 @@ map({ "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev sear
 map({ "i" }, "<C-z>", "<Esc><C-z>", { desc = "Close editor to background" })
 
 -- Live Grep
-map("n", "<leader>g", "<CMD>FzfxLiveGrep cword<CR>", { desc = "Live Grep word under cursor" })
+map("n", "<leader>g", "<CMD>FzfxLiveGrep cword<CR>", {silent = true, noremap = true, desc = "Live Grep word under cursor" })
 
 -- Telescope mapping
 map("n", "<C-p>", "<CMD>Telescope find_files<CR>", { desc = "Find files" })
