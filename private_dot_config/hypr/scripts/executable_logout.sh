cab3313f34119be38177270f5a90454f917c9359
Javier Tia <javier.tia@gmail.com>
2024-03-22 10:27:56 -0600

    hyprland: try new logout
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/hypr/scripts/executable_logout.sh
+++ private_dot_config/hypr/scripts/executable_logout.sh
@@ -3,4 +3,6 @@
 HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
 hyprctl --batch "${HYPRCMDS}" >> /tmp/hypr/hyprexitwithgrace.log 2>&1
 
-loginctl terminate-session "$(loginctl session-status | awk 'NR==1{print $1}')"
+sleep 1
+hyprctl dispatch exit
+loginctl kill-session "$(loginctl session-status | awk 'NR==1{print $1}')"
--- private_dot_config/hypr/scripts/executable_resetxdgportal.sh
+++ private_dot_config/hypr/scripts/executable_resetxdgportal.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gnome
 killall xdg-desktop-portal-kde
 killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 sleep 1
+
 /usr/lib/xdg-desktop-portal-hyprland &
+
 sleep 2
 /usr/lib/xdg-desktop-portal &
