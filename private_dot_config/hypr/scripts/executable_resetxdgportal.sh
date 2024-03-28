f83a652ec63bb3f8e7060c728247189b910953e2
Javier Tia <javier.tia@gmail.com>
2024-03-28 17:53:53 -0600

    hyprland: use gnome keyring
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/hypr/scripts/executable_resetxdgportal.sh
+++ private_dot_config/hypr/scripts/executable_resetxdgportal.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env sh
-
-killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall xdg-desktop-portal-wlr
-killall xdg-desktop-portal-gtk
-killall xdg-desktop-portal
+#!/usr/bin/sh
 sleep 1
+killall xdg-desktop-portal-gtk
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-lxqt
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal
 
 /usr/lib/xdg-desktop-portal-hyprland &
-
 sleep 2
 /usr/lib/xdg-desktop-portal &
