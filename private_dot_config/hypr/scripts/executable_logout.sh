17e5a20110c9bc7d9f19060bfea752caf72b7209
Javier Tia <javier.tia@gmail.com>
2024-04-01 13:51:27 -0600

    hyprland: rewrite logout
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/hypr/scripts/executable_logout.sh
+++ private_dot_config/hypr/scripts/executable_logout.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
-hyprctl --batch "${HYPRCMDS}" >> /tmp/hypr/hyprexitwithgrace.log 2>&1
-
-sleep 1
-hyprctl dispatch exit
-# loginctl kill-session "$(loginctl session-status | awk 'NR==1{print $1}')"
+if pgrep -x Hyprland >/dev/null; then
+    hyprctl dispatch exit 0
+    sleep 2
+    if pgrep -x Hyprland >/dev/null; then
+        killall -9 Hyprland
+    fi
+fi
