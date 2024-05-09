cea3306de9b6734ae2e107f465c1041cb08c4c0c
Javier Tia <javier.tia@gmail.com>
2024-05-08 18:06:08 -0600

    av1-compress: adjust params
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_bin/executable_av1-compress.sh
+++ exact_bin/executable_av1-compress.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+    # -pix_fmt yuv444p10le \
 ffmpeg -i "${1}" \
     -c:v libsvtav1 \
-    -crf 16 \
-    -pix_fmt yuv444p10le \
     -c:a copy \
+    -crf 5 \
+    -pix_fmt yuv420p10le \
     -svtav1-params tune=0 \
     -sn \
     "$(basename "${1}" .mkv)"_AV1.mkv
