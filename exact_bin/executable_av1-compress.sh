26c02a93fd8f94f1a0182979935f2dfb8f787392
Javier Tia <javier.tia@gmail.com>
2024-07-02 11:42:52 -0600

    av1-compress: use overall bitrate
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_bin/executable_av1-compress.sh
+++ exact_bin/executable_av1-compress.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-    # -pix_fmt yuv444p10le \
-ffmpeg -i "${1}" \
+v="${1}"
+
+# Keep same overall bit rate from original video
+n=$(ffmpeg -i "${v}" 2>&1 | awk '/bitrate/ {print $6}')
+r=$(((n + 700) / 1000))
+
+ffmpeg -i "${v}" \
     -c:v libsvtav1 \
-    -c:a copy \
-    -crf 5 \
+    -b:v "${r}M" \
     -pix_fmt yuv420p10le \
     -svtav1-params tune=0 \
     -sn \
-    "$(basename "${1}" .mkv)"_AV1.mkv
+    "$(basename "${v}" .mkv)"_AV1.mkv
