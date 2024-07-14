cbcf8c629c1b7a6d594d0518839d664905cb2558
Javier Tia <javier.tia@gmail.com>
2024-07-14 07:31:47 -0600

    nvim: disable nvim-spectre
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -175,16 +175,12 @@ return {
   },
 
   -- search/replace in multiple files
-  {
-    "nvim-pack/nvim-spectre",
-    build = false,
-    cmd = "Spectre",
-    opts = { open_cmd = "noswapfile vnew" },
-    -- stylua: ignore
-    keys = {
-      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
-    },
-  },
+  -- {
+  --   "nvim-pack/nvim-spectre",
+  --   build = false,
+  --   cmd = "Spectre",
+  --   opts = { open_cmd = "noswapfile vnew" },
+  -- },
 
   -- Automatic indentation style detection for Neovim
   -- {
