31a53b597154ece00d6f4477cb56665c882fb634
Javier Tia <javier.tia@gmail.com>
2024-10-09 17:52:22 -0600

    nvim: rework lazy config
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/lazy.lua
@@ -1,45 +1,7 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local luv = vim.uv or vim.loop -- TODO: REMOVE WHEN DROPPING SUPPORT FOR Neovim v0.9
-if not luv.fs_stat(lazypath) then
-  local output = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--branch=stable",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-  if vim.api.nvim_get_vvar("shell_error") ~= 0 then
-    vim.api.nvim_err_writeln(
-      "Error cloning lazy.nvim repository...\n\n" .. output
-    )
-  end
-  local oldcmdheight = vim.opt.cmdheight:get()
-  vim.opt.cmdheight = 1
-  vim.notify("Please wait while plugins are installed...")
-  vim.api.nvim_create_autocmd("User", {
-    desc = "Load Mason and Treesitter after Lazy installs plugins",
-    once = true,
-    pattern = "LazyInstall",
-    callback = function()
-      vim.cmd.bw()
-      vim.opt.cmdheight = oldcmdheight
-      vim.tbl_map(function(module)
-        pcall(require, module)
-      end, { "nvim-treesitter", "mason" })
-      vim.notify("Mason is installing packages if configured, check status with `:Mason`")
-    end,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
+return {
   defaults = {
     cond = not vim.g.vscode,
     version = false, -- always use the latest git commit
-  },
-  spec = {
-    { import = "plugins" },
   },
   checker = {
     enabled = true,
@@ -51,17 +13,34 @@ require("lazy").setup({
     rtp = {
       -- Internal Plugins at share/nvim/runtime/plugin
       disabled_plugins = {
-        "gzip",
-        "man",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "spellfile",
-        "tarPlugin",
+        "2html_plugin",
         "tohtml",
-        "tutor",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
         "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
       },
     },
   },
-})
+}
--- private_dot_config/exact_nvim/init.lua
+++ private_dot_config/exact_nvim/init.lua
@@ -11,10 +11,50 @@ vim.filetype.add({
   },
 })
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local luv = vim.uv
+if not luv.fs_stat(lazypath) then
+  local output = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+  if vim.api.nvim_get_vvar("shell_error") ~= 0 then
+    vim.api.nvim_err_writeln("Error cloning lazy.nvim repository...\n\n" .. output)
+  end
+  vim.notify("Please wait while plugins are installed...")
+  vim.api.nvim_create_autocmd("User", {
+    desc = "Load Mason and Treesitter after Lazy installs plugins",
+    once = true,
+    pattern = "LazyInstall",
+    callback = function()
+      vim.cmd.bw()
+      vim.tbl_map(function(module)
+        pcall(require, module)
+      end, { "nvim-treesitter", "mason" })
+      vim.notify("Mason is installing packages if configured, check status with `:Mason`")
+    end,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+
+local lazy_config = require("config.lazy")
+require("lazy").setup({
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
 for _, source in ipairs({
   "config.utils",
   "config.options",
-  "config.lazy",
   "config.autocmds",
   "config.mappings",
 }) do
