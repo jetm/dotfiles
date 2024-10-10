e920186c58afe9f59dfceef863845ecdaeb16a02
Javier Tia <javier.tia@gmail.com>
2024-10-09 18:14:09 -0600

    nvim: add mason.lua
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

---
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/mason.lua
@@ -0,0 +1,13 @@
+dofile(vim.g.base46_cache .. "mason")
+
+return {
+  ui = {
+    icons = {
+      package_pending = " ",
+      package_installed = " ",
+      package_uninstalled = " ",
+    },
+  },
+
+  max_concurrent_installers = 10,
+}
