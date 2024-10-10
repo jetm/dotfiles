97d948ac39de95b60efe55fcc39a1cacc699f03e
Javier Tia <javier.tia@gmail.com>
2024-10-09 18:17:51 -0600

    nvim: add cursor_position
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/chadrc.lua
+++ private_dot_config/exact_nvim/exact_lua/chadrc.lua
@@ -1,6 +1,5 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :(
 
 local M = {}
 M = {
@@ -21,34 +20,31 @@ M = {
       -- default/round/block/arrow separators work only for default statusline theme
       -- round and block will work for minimal theme only
       separator_style = "round",
-      -- modules = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor_position" },
-      overriden_modules = function()
-        local fn = vim.fn
-        local sep_l = ""
+      order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor_position" },
+      modules = {
+        cursor_position = function()
+          local fn = vim.fn
+          local sep_l = ""
 
-        return {
+          local line = fn.line(".")
+          local col = fn.virtcol(".")
 
-          cursor_position = function()
-            local line = fn.line(".")
-            local col = fn.virtcol(".")
+          local chad = string.format(" %d:%d", line, col) .. " │ "
 
-            local chad = string.format(" %d:%d", line, col) .. " │ "
+          -- default cursor_position module
+          local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
 
-            -- default cursor_position module
-            local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
+          local current_line = fn.line(".")
+          local total_line = fn.line("$")
+          local text = math.modf((current_line / total_line) * 100) .. tostring("%%")
+          text = string.format("%4s", text)
 
-            local current_line = fn.line(".")
-            local total_line = fn.line("$")
-            local text = math.modf((current_line / total_line) * 100) .. tostring("%%")
-            text = string.format("%4s", text)
+          text = (current_line == 1 and "Top") or text
+          text = (current_line == total_line and "Bot") or text
 
-            text = (current_line == 1 and "Top") or text
-            text = (current_line == total_line and "Bot") or text
-
-            return left_sep .. "%#St_pos_text#" .. chad .. text .. " "
-          end,
-        }
-      end,
+          return left_sep .. "%#St_pos_text#" .. chad .. text .. " "
+        end,
+      },
     },
 
     -- lazyload it when there are 1+ buffers
