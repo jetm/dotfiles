a81b2d149fe645eb648c9a4322caec83a4bf8df0
Javier Tia <javier.tia@gmail.com>
2024-10-09 18:13:55 -0600

    nvim: add lualine.lua
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

---
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lualine.lua
@@ -0,0 +1,78 @@
+return function(_, _)
+  local indent_size = function()
+    local indent_type = vim.api.nvim_buf_get_option(0, "expandtab") and "spaces" or "tab"
+    local indent_size = vim.api.nvim_buf_get_option(0, "tabstop")
+    return ("%s: %s"):format(indent_type, indent_size)
+  end
+
+  local lualine_require = require("lualine_require")
+  lualine_require.require = require
+
+  vim.o.laststatus = vim.g.lualine_laststatus
+
+  return {
+    options = {
+      theme = "auto",
+      globalstatus = true,
+      disabled_filetypes = { statusline = { "dashboard", "alpha", "starter", "neo-tree" } },
+    },
+    sections = {
+      lualine_a = { { "fancy_mode", width = 3 } },
+      lualine_b = { { "branch" } },
+
+      lualine_c = {
+        { "fancy_lsp_servers" },
+        { "fancy_diagnostics" },
+        { "fancy_filetype" },
+        { "ex.cwd" },
+        {
+          "ex.relative_filename",
+          shorten = {
+            -- count of letters, which will be taken from every part of the path
+            lenght = 1,
+          },
+        },
+      },
+
+      lualine_x = {
+        {
+          "fancy_diff",
+          source = function()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+              return {
+                added = gitsigns.added,
+                modified = gitsigns.changed,
+                removed = gitsigns.removed,
+              }
+            end
+          end,
+        },
+        {
+          -- 'encoding': Don't display if encoding is UTF-8
+          function()
+            local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
+            return ret
+          end,
+        },
+        {
+          -- fileformat: Don't display if unix
+          function()
+            local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
+            return ret
+          end,
+        },
+      },
+
+      lualine_y = {
+        indent_size,
+      },
+
+      lualine_z = {
+        { "ex.progress" },
+        { "ex.location", pattern = "%3L:%-2C" },
+      },
+    },
+    extensions = { "neo-tree" },
+  }
+end
