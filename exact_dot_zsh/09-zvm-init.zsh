e0abd4afc71468e4b06bf5a48046787328438063
Javier Tia <javier.tia@gmail.com>
2024-10-23 11:38:23 -0600

    zsh: allow to yank text in vi-mode
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_dot_zsh/09-zvm-init.zsh
+++ exact_dot_zsh/09-zvm-init.zsh
@@ -1,6 +1,63 @@
 #
 # zsh-vi-mode
 #
+
+cb_copy() {
+    copyq copy - > /dev/null
+}
+cb_paste() {
+    copyq clipboard
+}
+
+my_zvm_vi_yank() {
+    zvm_vi_yank
+    echo -en "$CUTBUFFER" | cb_copy
+}
+
+my_zvm_vi_delete() {
+    zvm_vi_delete
+    echo -en "$CUTBUFFER" | cb_copy
+}
+
+my_zvm_vi_change() {
+    zvm_vi_change
+    echo -en "$CUTBUFFER" | cb_copy
+}
+
+my_zvm_vi_change_eol() {
+    zvm_vi_change_eol
+    echo -en "$CUTBUFFER" | cb_copy
+}
+
+my_zvm_vi_put_after() {
+    CUTBUFFER=$(cb_paste)
+    zvm_vi_put_after
+    zvm_highlight clear # zvm_vi_put_after introduces weird highlighting for me
+}
+
+my_zvm_vi_put_before() {
+    CUTBUFFER=$(cb_paste)
+    zvm_vi_put_before
+    zvm_highlight clear # zvm_vi_put_before introduces weird highlighting for me
+}
+
+zvm_after_lazy_keybindings() {
+    zvm_define_widget my_zvm_vi_yank
+    zvm_define_widget my_zvm_vi_delete
+    zvm_define_widget my_zvm_vi_change
+    zvm_define_widget my_zvm_vi_change_eol
+    zvm_define_widget my_zvm_vi_put_after
+    zvm_define_widget my_zvm_vi_put_before
+
+    zvm_bindkey visual 'y' my_zvm_vi_yank
+    zvm_bindkey visual 'd' my_zvm_vi_delete
+    zvm_bindkey visual 'x' my_zvm_vi_delete
+    zvm_bindkey vicmd 'C' my_zvm_vi_change_eol
+    zvm_bindkey visual 'c' my_zvm_vi_change
+    zvm_bindkey vicmd 'p' my_zvm_vi_put_after
+    zvm_bindkey vicmd 'P' my_zvm_vi_put_before
+}
+
 # zsh-vi-mode will auto execute this zvm_after_init function
 zvm_after_init() {
   # Require load it here as it's
