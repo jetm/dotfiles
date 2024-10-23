b5c1b31bad923ad5b794e9a232100716df9f81f4
Javier Tia <javier.tia@gmail.com>
2024-10-23 11:40:03 -0600

    zsh: text formatting
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- exact_dot_zsh/09-zvm-init.zsh
+++ exact_dot_zsh/09-zvm-init.zsh
@@ -60,20 +60,20 @@ zvm_after_lazy_keybindings() {
 
 # zsh-vi-mode will auto execute this zvm_after_init function
 zvm_after_init() {
-  # Require load it here as it's
-  source "${HOME}"/.zsh/00-utility.zsh
+    # Require load it here as it's
+    source "$HOME"/.zsh/00-utility.zsh
 
-  _distro=$(lsb_release -si)
-  if [ "$_distro" = "Fedora" ]; then
-    source /usr/share/fzf/shell/key-bindings.zsh
-  else
-    source /usr/share/fzf/key-bindings.zsh
-  fi
-  unset _distro
+    _distro=$(lsb_release -si)
+    if [ "$_distro" = "Fedora" ]; then
+        source /usr/share/fzf/shell/key-bindings.zsh
+    else
+        source /usr/share/fzf/key-bindings.zsh
+    fi
+    unset _distro
 
-  source "${HOME}"/.zsh/key-bindings.sh
+    source "$HOME"/.zsh/key-bindings.sh
 
-  source "${HOME}"/.zsh/kitty-shell-integration.sh
+    source "$HOME"/.zsh/kitty-shell-integration.sh
 }
 
 # vim:set ts=2 sw=2 et:
