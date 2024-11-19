e4f2597f8a87e42f620ddacf908e8c6793dc38da
Javier Tia <javier.tia@gmail.com>
2024-11-19 12:08:41 -0600

    nvim: Restore autopair.nvim
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -719,24 +719,12 @@ return {
     },
   },
 
-  -- Vim plugin, insert or delete brackets, parens, quotes in pair
-  -- {
-  --   "windwp/nvim-autopairs",
-  --   dependencies = {
-  --     { "hrsh7th/nvim-cmp" },
-  --   },
-  --   opts = {
-  --     fast_wrap = {},
-  --     disable_filetype = { "TelescopePrompt", "vim" },
-  --   },
-  --   config = function(_, opts)
-  --     require("nvim-autopairs").setup(opts)
-  --
-  --     -- setup cmp for autopairs
-  --     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-  --     require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  --   end,
-  -- },
+  -- autopairs for neovim written in lua
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
 
   {
     "nvim-telescope/telescope-fzf-native.nvim",
