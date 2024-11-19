30f692dd7883fbacbcf5a721ff8f90ec222ac3b4
Javier Tia <javier.tia@gmail.com>
2024-11-19 12:09:05 -0600

    nvim: Disable avante.nvim
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -873,69 +873,69 @@ return {
   },
 
   -- Use your Neovim like using Cursor AI IDE
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "openai",
-      -- auto_suggestions_provider = "claude",
-      openai = {
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
-        temperature = 0,
-        max_tokens = 4096,
-      },
-      behaviour = {
-        auto_suggestions = false, -- Experimental stage
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-      },
-      mappings = {
-        --- @class AvanteConflictMappings
-        diff = {
-          ours = "co",
-          theirs = "ct",
-          all_theirs = "ca",
-          both = "cb",
-          cursor = "cc",
-          next = "]x",
-          prev = "[x",
-        },
-        suggestion = {
-          accept = "<A-m>",
-          next = "<C-]>",
-          prev = "<C-[>",
-          dismiss = "<C-S-]>",
-        },
-        jump = {
-          next = "]]",
-          prev = "[[",
-        },
-        submit = {
-          normal = "<CR>",
-          insert = "<C-s>",
-        },
-        sidebar = {
-          apply_all = "A",
-          apply_cursor = "a",
-          switch_windows = "<Tab>",
-          reverse_switch_windows = "<S-Tab>",
-        },
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     provider = "openai",
+  --     -- auto_suggestions_provider = "claude",
+  --     openai = {
+  --       endpoint = "https://api.deepseek.com",
+  --       model = "deepseek-coder",
+  --       temperature = 0,
+  --       max_tokens = 4096,
+  --     },
+  --     behaviour = {
+  --       auto_suggestions = false, -- Experimental stage
+  --       auto_set_highlight_group = true,
+  --       auto_set_keymaps = true,
+  --       auto_apply_diff_after_generation = false,
+  --       support_paste_from_clipboard = false,
+  --     },
+  --     mappings = {
+  --       --- @class AvanteConflictMappings
+  --       diff = {
+  --         ours = "co",
+  --         theirs = "ct",
+  --         all_theirs = "ca",
+  --         both = "cb",
+  --         cursor = "cc",
+  --         next = "]x",
+  --         prev = "[x",
+  --       },
+  --       suggestion = {
+  --         accept = "<A-m>",
+  --         next = "<C-]>",
+  --         prev = "<C-[>",
+  --         dismiss = "<C-S-]>",
+  --       },
+  --       jump = {
+  --         next = "]]",
+  --         prev = "[[",
+  --       },
+  --       submit = {
+  --         normal = "<CR>",
+  --         insert = "<C-s>",
+  --       },
+  --       sidebar = {
+  --         apply_all = "A",
+  --         apply_cursor = "a",
+  --         switch_windows = "<Tab>",
+  --         reverse_switch_windows = "<S-Tab>",
+  --       },
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --   },
+  -- },
 
   -- {
   --   "Exafunction/codeium.vim",
