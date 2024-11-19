61e513dea9319877aae90443ea8912345afdd132
Javier Tia <javier.tia@gmail.com>
2024-11-19 12:07:51 -0600

    nvim: add snacks.nvim
    Signed-off-by: Javier Tia <javier.tia@gmail.com>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -30,6 +30,23 @@ return {
     config = true,
   },
 
+  -- A collection of small QoL plugins for Neovim
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+
   -- {
   --   "navarasu/onedark.nvim",
   --   lazy = false,
