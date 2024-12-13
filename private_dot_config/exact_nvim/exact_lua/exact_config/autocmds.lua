793c25868dee695340a7455e4a355cbf8345cd2d
Javier Tia <jetm@jetm.me>
2024-12-13 11:42:27 -0600

    nvim: update close_q autocmd
    Signed-off-by: Javier Tia <jetm@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -156,21 +156,37 @@ autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
     "PlenaryTestPopup",
-    "nofile",
+    "checkhealth",
+    "dbout",
     "floaterm",
+    "gitsigns-blame",
+    "grug-far",
     "help",
     "lspinfo",
     "man",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "nofile",
     "notify",
     "qf",
-    "query", -- :InspectTree
+    "query",
     "spectre_panel",
     "startuptime",
     "tsplayground",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = event.buf, silent = true })
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
 
