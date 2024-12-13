59e3938c40c93665cc44ef84157ccf97b1c60629
Javier Tia <jetm@jetm.me>
2024-12-13 11:43:40 -0600

    nvim: add vscode mappings
    Signed-off-by: Javier Tia <jetm@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_config/vscode_keymaps.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/vscode_keymaps.lua
@@ -18,3 +18,16 @@ keymap("v", "p", '"_dP', opts)
 
 keymap({"n", "v"}, "<leader>f", "<cmd>lua require('vscode').action('editor.action.formatDocument')<CR>")
 keymap({"n", "v"}, "gq", "<cmd>lua require('vscode').action('rewrap.rewrapComment')<CR>")
+
+-- VSCode-specific keymaps for search and navigation
+keymap("n", "<leader><space>", "<cmd>Find<cr>")
+keymap("n", "<leader>/", [[<cmd>lua require('vscode').action('workbench.action.findInFiles')<CR>]])
+keymap("n", "<leader>ss", [[<cmd>lua require('vscode').action('workbench.action.gotoSymbol')<CR>]])
+
+-- Keep undo/redo lists in sync with VsCode
+keymap("n", "u", "<Cmd>call VSCodeNotify('undo')<CR>")
+keymap("n", "<C-r>", "<Cmd>call VSCodeNotify('redo')<CR>")
+
+-- Navigate VSCode tabs
+keymap("n", "<M-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+keymap("n", "<M-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
