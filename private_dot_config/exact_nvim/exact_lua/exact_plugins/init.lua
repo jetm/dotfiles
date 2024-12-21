82656482ce9255c8c33a0a750cc572ba17c01d89
Javier Tia <jetm@jetm.me>
2024-12-20 18:25:24 -0600

    nvim: refresh blink.cmp settings
    Signed-off-by: Javier Tia <jetm@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -37,14 +37,9 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       bigfile = { enabled = true },
       notifier = { enabled = true },
       quickfile = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
     },
     keys = {
       {
@@ -1056,7 +1051,7 @@ return {
           selection = "manual",
         },
         menu = {
-          draw = { treesitter = true },
+          draw = { treesitter = {"lsp"} },
         },
       },
     },
