b2e38c476a513ac61cd6be48908c6c54d6baa3ed
Javier Tia <jetm@jetm.me>
2025-01-13 18:32:25 -0600

    hyprland: change to KDE
    Signed-off-by: Javier Tia <jetm@jetm.me>

--- private_dot_config/exact_hypr/exact_scripts/executable_resetxdgportal.sh
+++ private_dot_config/exact_hypr/exact_scripts/executable_resetxdgportal.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+
 sleep 1
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal-gnome
@@ -10,6 +11,9 @@ killall xdg-desktop-portal
 logger 'killed all xdg-desktop'
 /usr/lib/xdg-desktop-portal-gtk &
 logger 'xdg-desktop-portal-gtk started'
+sleep 1
+/usr/lib/xdg-desktop-portal-kde &
+logger 'xdg-desktop-portal-kde started'
 sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
 logger 'xdg-desktop-portal-hyprland started'
