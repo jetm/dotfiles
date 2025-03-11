22d905474777083a8421b5836a4a8fd8a4b97985
Javier Tia <floss@jetm.me>
2025-03-11 08:08:10 -0600

    nvim: fix grug deprecated function
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -330,7 +330,7 @@ return {
         "<leader>g",
         function()
           local grug = require("grug-far")
-          grug.grug_far({
+          grug.open({
             prefills = {
               search = vim.fn.expand("<cword>"),
             },
@@ -344,7 +344,7 @@ return {
         function()
           local grug = require("grug-far")
           local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-          grug.grug_far({
+          grug.open({
             prefills = {
               filesFilter = ext and ext ~= "" and "*." .. ext or nil,
             },
