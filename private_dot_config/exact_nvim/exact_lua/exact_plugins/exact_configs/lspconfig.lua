b4681de768e9ed9ee81f6536559e57d5c6213f7a
Javier Tia <floss@jetm.me>
2025-03-12 08:30:37 -0600

    nvim: disable harper_ls
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lspconfig.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/exact_configs/lspconfig.lua
@@ -49,26 +49,25 @@ return function(_, _)
       })
     end,
 
-    ["harper_ls"] = function()
-      lspconfig.harper_ls.setup({
-        cmd = { "harper-ls", "--stdio" },
-        filetypes = { "gitcommit", "markdown", "text" },
-        settings = {
-          ["harper-ls"] = {
-            codeActions = {
-              forceStable = true,
-            },
-            -- typos handles spell checking
-            linters = {
-              sentence_capitalization = false,
-              spell_check = false,
-            },
-            userDictPath = vim.env.XDG_CONFIG_HOME .. "/harper/dict.txt",
-          },
-        },
-        single_file_support = true,
-      })
-    end,
+    -- Disable it from now. Too slow
+    -- ["harper_ls"] = function()
+    --   lspconfig.harper_ls.setup({
+    --     filetypes = { "gitcommit", "markdown", "text" },
+    --     settings = {
+    --       ["harper-ls"] = {
+    --         codeActions = {
+    --           forceStable = true,
+    --         },
+    --         -- typos handles spell checking
+    --         linters = {
+    --           sentence_capitalization = false,
+    --           spell_check = false,
+    --         },
+    --       },
+    --     },
+    --     single_file_support = true,
+    --   })
+    -- end,
 
     ["clangd"] = function()
       lspconfig.clangd.setup({
