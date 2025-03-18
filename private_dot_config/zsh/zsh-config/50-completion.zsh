afd00433deb65688595fa3f2332a7c6379ee0769
Javier Tia <floss@jetm.me>
2025-03-17 19:58:02 -0600

    zsh: git fixup completion
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/zsh/zsh-config/50-completion.zsh
+++ private_dot_config/zsh/zsh-config/50-completion.zsh
@@ -171,4 +171,7 @@ zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' l
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
 
+# git-fixup
+zstyle ':completion:*:*:git:*' user-commands fixup:'Create a fixup commit'
+
 source <(delta --generate-completion zsh)
