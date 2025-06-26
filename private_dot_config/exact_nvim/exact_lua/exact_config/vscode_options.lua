7be117d5e133fd8738847947653f173b736bf0d4
Javier Tia <floss@jetm.me>
2025-06-26 08:50:28 -0600

    nvim: vscode: WAR for UI desync
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_config/vscode_options.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_config/vscode_options.lua
@@ -81,3 +81,25 @@ vim.cmd([[
     autocmd TextYankPost * silent! lua vim.highlight.on_yank()
   augroup end
 ]])
+
+-- Workaround for vscode-neovim UI desync (issue #2117)
+
+-- 1. Redraw on CursorHold (idle for some time)
+local redraw_fix = vim.api.nvim_create_augroup('VSCodeRedrawFix', { clear = true })
+vim.api.nvim_create_autocmd('CursorHold', {
+  group = redraw_fix,
+  callback = function()
+    vim.cmd('silent! mode')  -- triggers a lightweight redraw
+  end,
+})
+
+-- 2. Redraw immediately after text changes (e.g., visual delete)
+local redraw_group = vim.api.nvim_create_augroup('RedrawOnDelete', { clear = true })
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  group = redraw_group,
+  callback = function()
+    if vim.fn.mode() == 'n' then
+      vim.cmd('silent! mode')  -- refresh UI after delete/insert
+    end
+  end,
+})
