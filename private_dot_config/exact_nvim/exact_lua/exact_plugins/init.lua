530647ae4d0a01c5c63e821be1fe91e213af9e1d
Javier Tia <floss@jetm.me>
2026-01-26 15:31:28 -0600

    nvim: add yamllint
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_plugins/init.lua
@@ -1133,7 +1133,7 @@ return {
             "yamlfmt",
 
             -- Linters
-            -- "yamllint",
+            "yamllint",
             "luacheck",
             "shellcheck",
           },
