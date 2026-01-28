019a20d383766c3d57cf9b1251525632044e7fb5
Javier Tia <floss@jetm.me>
2026-01-28 08:51:22 -0600

    zsh: clean unused files
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/zsh/zsh-config/kitty-shell-integration.sh
+++
@@ -1,8 +0,0 @@
-if test -n "$KITTY_INSTALLATION_DIR"; then
-  export KITTY_SHELL_INTEGRATION="enabled"
-  autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-  kitty-integration
-  unfunction kitty-integration
-fi
-
-# vim:ft=zsh ts=2 sw=2 et:
