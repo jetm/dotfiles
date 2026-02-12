f454f67fea5eb2ba31e354870813e42a7837f476
Javier Tia <floss@jetm.me>
2026-02-12 14:10:15 -0600

    private_dot_config/kitty: Format code with ruff
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/kitty/smart_scroll.py
+++ private_dot_config/kitty/smart_scroll.py
@@ -1,4 +1,3 @@
-from typing import Iterator
 
 import kitty.key_encoding as ke
 
@@ -21,14 +20,32 @@ def handle_result(args, result, target_window_id, boss):
 
     mods, key = ke.parse_shortcut(args[2])
     shift, alt, ctrl, super, hyper, meta, caps_lock, num_lock = (
-        bool(mods & bit) for bit in (
-           ke.SHIFT, ke.ALT, ke.CTRL, ke.SUPER,
-           ke.HYPER, ke.META, ke.CAPS_LOCK, ke.NUM_LOCK))
+        bool(mods & bit)
+        for bit in (
+            ke.SHIFT,
+            ke.ALT,
+            ke.CTRL,
+            ke.SUPER,
+            ke.HYPER,
+            ke.META,
+            ke.CAPS_LOCK,
+            ke.NUM_LOCK,
+        )
+    )
     for action in [ke.EventType.PRESS, ke.EventType.RELEASE]:
         key_event = ke.KeyEvent(
-            type=action, mods=mods, key=key,
-            shift=shift, alt=alt, ctrl=ctrl, super=super,
-            hyper=hyper, meta=meta, caps_lock=caps_lock, num_lock=num_lock)
+            type=action,
+            mods=mods,
+            key=key,
+            shift=shift,
+            alt=alt,
+            ctrl=ctrl,
+            super=super,
+            hyper=hyper,
+            meta=meta,
+            caps_lock=caps_lock,
+            num_lock=num_lock,
+        )
         window_system_event = key_event.as_window_system_event()
         sequence = w.encoded_key(window_system_event)
         w.write_to_child(sequence)
