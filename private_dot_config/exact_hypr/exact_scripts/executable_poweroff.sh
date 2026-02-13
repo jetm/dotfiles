2db39b6498f594d9712169659d9aee13d96a4f19
Javier Tia <floss@jetm.me>
2026-02-12 18:06:24 -0600

    dotfiles: Clean up Hyprland keybinds and remove unused scripts
    Signed-off-by: Javier Tia <floss@jetm.me>

--- private_dot_config/exact_hypr/exact_scripts/executable_poweroff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
-hyprctl --batch "${HYPRCMDS}" >>/tmp/hypr/hyprexitwithgrace.log 2>&1
-
-systemctl poweroff
